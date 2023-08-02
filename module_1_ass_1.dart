99207da7ab486bac0e75b7cb1105d5274cebe70d
Tangim <tangim15-4103@diu.edu.bd>
2023-08-03 01:45:35 +0600

    done

--- module_1_ass_1.dart
+++ module_1_ass_1.dart
@@ -1,22 +1,22 @@
-void main(){
-   const num a = 20;
-   const num b = 11;
+void main() {
+  const num a = 20;
+  const num b = 11;
 
-   print("a = ${a}, b =${b}");
-   print("Addition: ${a+b}");
-   print("Substraction: ${a-b}");
-   print("Multiplication: ${a*b}");
-   print("Division: ${a/b}");
-   print("Modulus: ${a%b}");
+  print("a = ${a}, b =${b}");
+  print("Addition: ${a} + ${b} = ${a + b}");
+  print("Substraction: ${a} - ${b} = ${a - b}");
+  print("Multiplication: ${a} * ${b} = ${a * b}");
+  print("Division: ${a} / ${b} = ${a / b}");
+  print("Modulus: ${a} % ${b} = ${a % b}");
 }
 
 
 /*
 Output:- 
 a = 20, b =11
-Addition: 31
-Substraction: 9
-Multiplication: 220
-Division: 1.8181818181818181
-Modulus: 9
+Addition: 20 + 11 = 31
+Substraction: 20 - 11 = 9
+Multiplication: 20 * 11 = 220
+Division: 20 / 11 = 1.8181818181818181
+Modulus: 20 % 11 = 9
  */
