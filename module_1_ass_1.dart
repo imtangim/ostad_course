65ee3877f9d7e4d847ab71c145b1af333538dc4e
Tangim <tangim15-4103@diu.edu.bd>
2023-08-02 20:14:49 +0600

    Assignment done

---
+++ module_1_ass_1.dart
@@ -0,0 +1,10 @@
+void main(){
+   const num a = 20;
+   const num b = 11;
+
+   print("Addition: ${a+b}");
+   print("Substraction: ${a-b}");
+   print("Multiplication: ${a*b}");
+   print("Division: ${a/b}");
+   print("Modulus: ${a%b}");
+}
