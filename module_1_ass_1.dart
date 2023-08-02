413d69aa354c6e2eedc5f62eb6ecb90aeea4a2b1
Tangim <tangim15-4103@diu.edu.bd>
2023-08-02 20:17:38 +0600

    Done

--- module_1_ass_1.dart
+++ module_1_ass_1.dart
@@ -2,9 +2,21 @@ void main(){
    const num a = 20;
    const num b = 11;
 
+   print("a = ${a}, b =${b}");
    print("Addition: ${a+b}");
    print("Substraction: ${a-b}");
    print("Multiplication: ${a*b}");
    print("Division: ${a/b}");
    print("Modulus: ${a%b}");
 }
+
+
+/*
+Output:- 
+a = 20, b =11
+Addition: 31
+Substraction: 9
+Multiplication: 220
+Division: 1.8181818181818181
+Modulus: 9
+ */
