5e058d04bb1d2145787aa9c34224cc336ef5d6b5
Tangim <tangim15-4103@diu.edu.bd>
2023-09-23 02:59:28 +0600

    Assignment 7 done

---
+++ ass_7/README.md
@@ -0,0 +1,3 @@
+Homepage![App Screenshot](./assets/1.png)
+Dialog Box![App Screenshot](./assets/2.png)
+Cart![App Screenshot](./assets/3.png)
---
+++ ass_7/lib/main.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Product List"),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: [
+          ListView.builder(
+            itemCount: 30,
+            itemBuilder: (context, index) {
+              return ListTile(
+                dense: true,
+                title: Text("Product ${index + 1}"),
+                subtitle: const Text("\$20.00"),
+                trailing: Column(
+                  children: [
+                    Text(
+                      "Counter: $_counter",
+                      style: const TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    Expanded(
+                      child: TextButton(
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                elevation: 10,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                backgroundColor: Colors.white,
+                                title: const Text(
+                                  "Congratulation!",
+                                ),
+                                content: Text(
+                                  "You've bought Product ${index + 1}",
+                                  style: Theme.of(context).textTheme.bodyLarge,
+                                ),
+                                actionsPadding: const EdgeInsets.all(0),
+                                actionsAlignment: MainAxisAlignment.end,
+                                actions: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8.0),
+                                    child: TextButton(
+                                        style: TextButton.styleFrom(
+                                          shape: const RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.all(
+                                              Radius.circular(10),
+                                            ),
+                                          ),
+                                        ),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text(
+                                          "Ok",
+                                          style: TextStyle(
+                                            color: Colors.blue,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        )),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                          setState(() {
+                            _counter += 1;
+                          });
+                        },
+                        style: TextButton.styleFrom(
+                          fixedSize: const Size(100, 30),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          backgroundColor: Colors.blue,
+                        ),
+                        child: const Text(
+                          "Buy Now",
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: IconButton(
+                style: IconButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  fixedSize: const Size.fromRadius(28),
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return MyWidget(counter: _counter);
+                      },
+                    ),
+                  );
+                },
+                icon: const Icon(
+                  Icons.shopping_cart,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  final int counter;
+  const MyWidget({super.key, required this.counter});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cart"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Text(
+          "Total Product: ${widget.counter}",
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
